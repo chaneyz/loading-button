4fcf7a91b578de4715e02e576825e2044594eba5
Zach Chaney <zachary.chaney@gmail.com>
2015-05-27 23:13:14 -0500

    added button

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,4 +1,47 @@
-$(document).ready(function start(e) {
-	console.log(start);
+$(document).ready(function () {
+	
+	var $btn = $('#button')
+
+
+	$btn.on('click', loadBtn);
+
+	function loadBtn(e) {
+		$btn.html('Loading...');
+		$btn.css('color', '#cccccc');
+
+		setTimeout(submitBtn, 2000);
+	}
+
+	function submitBtn(e) {
+		$btn.html('Submit');
+		$btn.css('color', '#ffffff');
+	}
 
 });
+
+
+
+
+
+// $btn = $('#btn');
+// 	txtVal = $btn.val();
+
+// 	$btn.on('click', load);
+
+// 	function load(e){
+
+		
+// 		$btn.html('loading...') 
+// 		$btn.css('color','#cccccc');
+
+// 		setTimeout(changeBack, 4000);
+// 	}
+
+	
+
+// 	function changeBack(){
+
+// 		$btn.html('Submit') 
+// 		$btn.css('color','#000000');
+// 		btn.html = txtVal;
+// 	}
